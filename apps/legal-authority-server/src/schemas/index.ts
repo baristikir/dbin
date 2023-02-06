76a91fbcde1c606efeff6bc74b4338b365cdc4d3
Baris Tikir <brstkr@icloud.com>
2023-02-06 20:17:34 +0100

    Add Local Schema Registy Info (Development Environment)

--- apps/legal-authority-server/src/schemas/index.ts
+++ apps/legal-authority-server/src/schemas/index.ts
@@ -1,17 +1,29 @@
 import { writeFileSync } from "node:fs";
-import schemas from "./schema.json";
+import prodSchema from "./schema-prod.json";
+import localSchema from "./schema-local.json";
 
-let schemaId: string | undefined = schemas.schemaId ?? undefined;
-let credentialDefinitionId: string | undefined =
-	schemas.credentialDefinitionId ?? undefined;
+let schemaId: string | undefined = undefined;
+let credentialDefinitionId: string | undefined = undefined;
+
+if (process.env.NODE_ENV === "production") {
+	schemaId = prodSchema.schemaId;
+	credentialDefinitionId = prodSchema.credentialDefinitionId;
+} else {
+	schemaId = localSchema.schemaId;
+	credentialDefinitionId = localSchema.credentialDefinitionId;
+}
 
 export function saveSchemaId(newSchemaId: string) {
+	let schemas = process.env.NODE_ENV === "production" ? prodSchema : localSchema;
+
 	schemaId = newSchemaId;
 	const updatedSchema = { ...schemas, schemaId };
 	writeFileSync("schema.json", JSON.stringify(updatedSchema, null, 2));
 }
 
 export function saveCredentialDefinitionId(newCredentialDefinitionId: string) {
+	let schemas = process.env.NODE_ENV === "production" ? prodSchema : localSchema;
+
 	credentialDefinitionId = newCredentialDefinitionId;
 	const updatedSchemas = { ...schemas, credentialDefinitionId };
 	writeFileSync("schema.json", JSON.stringify(updatedSchemas, null, 2));
