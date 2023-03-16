b86a4db56a2a2b02f48925875e262c88338de377
Baris Tikir <brstkr@icloud.com>
2023-03-16 12:52:04 +0100

    Remove Client side authentication

--- apps/acme-web/src/pages/index.tsx
+++ apps/acme-web/src/pages/index.tsx
@@ -1,18 +1,6 @@
 import { LoadingSection } from "@dbin/ui";
-import { GetServerSideProps } from "next";
 import { Suspense } from "react";
 import { HomeView } from "../components/Home";
-import { authenticatedRoute } from "../utils/authRoutes";
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const authentication = await authenticatedRoute(ctx);
-
-	if ("redirect" in authentication) {
-		return authentication;
-	}
-
-	return { props: {} };
-};
 
 export default function Page() {
 	return (
